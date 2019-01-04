e3660a5cb9f946933d6468d504fa4e3d6a8693a6
aapicella <aapicella@gmail.com>
2019-01-04 09:15:21 -0500

    Simple blink on pin4 to show if clock set right
    The led on pin 4 should blink every second. If not then "Burn BootLoader"

---
+++ ATTINY85-Blink.ino
@@ -0,0 +1,20 @@
+/*
+ * This blink program is for ATTINY85
+ * if the LED blinks every 8 seconds you need to 
+ * burn the bootloader to use 8MHz clock.
+ */
+
+const int LED=4;
+
+void setup() {
+  //initialize pin 4
+  pinMode(LED, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(LED, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                       // wait for a second
+  digitalWrite(LED, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);                       // wait for a second
+}
